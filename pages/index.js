cf38efa076873227c21940764c9a502bd21c79ee
Binatik <47430210+Binatik@users.noreply.github.com>
2020-11-23 22:11:55 +0300

    fix async

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Head from 'next/head'
 
 //component
 import {Form} from '../components/form/form.js'
 
+async function getData(url) {
+    const data = await fetch(url);
+    return data.json()
+}
 
 function Index() {
+    const [data, setData] = useState(undefined);
 
-    async function getData(url) {
-        const data = await fetch(url);
-        return data.json()
-    }
-
-    let [ data, setData ] = useState()
-    setData(prev => {
-        return await getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json');
-    })
-    console.log(data);
+    useEffect(() => {
+        async function promiseDate() { 
+            const fetchDate = await getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json');
+            setData(fetchDate);
+        }
+        promiseDate().then(r => r);
+    },[data]);
+    //setData(async(prev) => prev + await getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json'));
 
     return (
         <React.Fragment>
@@ -24,7 +27,6 @@ function Index() {
                 <title>Form</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
-            {data}
             <Form data={data}/>
         </React.Fragment>
     )
