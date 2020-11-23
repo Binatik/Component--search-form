8fb18f965f2cd722d2b3b6e77a8cf926ce298863
Binatik <47430210+Binatik@users.noreply.github.com>
2020-11-23 15:20:40 +0300

    bag Index  AND Async function

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,25 @@ import Head from 'next/head'
 //component
 import {Form} from '../components/form/form.js'
 
-function Index() {
-    return async function main() {
+async function Index() {
 
-        async function getData(url) {
-            const data = await fetch(url);
-            return data.json()
-        }
-
-        const data = await getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json');
-
-        return (
-            <React.Fragment>
-                <Head>
-                    <title>Form</title>
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                </Head>
-                {data}
-                <Form data={data}/>
-            </React.Fragment>
-        )
+    async function getData(url) {
+        const data = await fetch(url);
+        return data.json()
     }
+
+    const data = await getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json');
+
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Form</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            </Head>
+            {data}
+            <Form data={data}/>
+        </React.Fragment>
+    )
 }
 
 export default Index
