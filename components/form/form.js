f9b1d56d5647f98b8e8223384af4db3f9e667e9c
Binatik <47430210+Binatik@users.noreply.github.com>
2020-11-23 09:47:55 +0300

    add  component

---
+++ components/form/form.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export function Form({data}) {
+    return (
+        <React.Fragment>
+            <form>
+                <h2>Критерии поиска: не установлены</h2>
+            </form>
+        </React.Fragment>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Head from 'next/head'
 
+//component
+import {Form} from '../components/form/form.js'
+
 async function main(){
 
     async function getData(url) {
@@ -8,20 +11,21 @@ async function main(){
         return data.json()
     }
 
-
     function Index() {
-        const data = getData('');
+        const data = getData('https://raw.githubusercontent.com/Binatik/SearchForm/main/data.json');
         return (
             <React.Fragment>
                 <Head>
                     <title>Form</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 </Head>
-                <form data = {}></form>
+                <Form data = {data}/>
             </React.Fragment>
         )
     }
-    export default Index
-}
 
-main();
+    export default Index
+
+
+}
+main().then(r => r); //Нужно уйти по делам. 
