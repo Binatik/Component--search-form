a632c238f9028d74d158c0f265e1770a8b819283
Сергей Александрович <47430210+Binatik@users.noreply.github.com>
2021-01-18 08:04:09 +0300

    Add files via upload

---
+++ _next/static/sqUkHedaWzR_NSt7pOF1w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F41f9ee8c0273d4279f91.css","static\u002Fchunks\u002Fpages\u002Findex-ca1dd01b2ddc46fed305.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b8c50c78660f3a11378a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
